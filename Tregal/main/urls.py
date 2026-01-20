c3007813e4fc44a426891ecf2f1159895d527703
Emmanuel Aduah <aduahimma18@gmail.com>
2026-01-21 02:20:57 +0400

    Modified our services and trending package

--- Tregal/main/urls.py
+++ Tregal/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
+    path('services/', services, name='services'),
 ]
